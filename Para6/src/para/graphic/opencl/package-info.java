27721f2de068cec2a9cd2f027e078604fd73738c
guben <japan3bulls@gmail.com>
2025-04-23 14:27:43 +0900

    lesson6 gpu computing

---
+++ Para6/src/para/graphic/opencl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * OpenCLを図形表示のパッケージの枠組みの中で利用するクラスのパッケージ。<br>
+ * OpenCLのjava bindingであるJOCLの初期設定部分を簡略に行えるようにしたEasyCLライブラリを使う実装になっている。
+ * @see <a href="../../../easycl/easycl/CLSetup.html">easycl.CLSetup</a>
+ */
+package para.graphic.opencl;
---
+++ Para6/src/para/opencl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * OpenCL利用クラスのパッケージ
+ * @see <a href=../../easycl/index.html>easyclパッケージ</a>
+ */
+package para.opencl;
